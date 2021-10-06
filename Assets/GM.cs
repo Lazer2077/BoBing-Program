b15e5ff2a62dc7dcb7c1e015b09b9407648d5951
“Lazer2077” <709245585@qq.com>
2021-10-06 22:20:05 +0800

    First UI

---
+++ Assets/GM.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GM : MonoBehaviour
+{
+    // Start is called before the first frame update
+   public void OnStartGame(){
+       SceneManager.LoadScene("New Scene");
+
+   }
+}
---
+++ Assets/Music1.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Music1 : MonoBehaviour
+{
+    private AudioSource audio;//定义声音组件
+    public AudioClip Music;//放置音
+    public Slider slider;
+    // Start is called before the first frame update
+    void Start()
+    {
+           audio = this.GetComponent<AudioSource>();//得到声音组件
+    }
+     void Update()
+    {
+        audio.volume=slider.value;
+    }
+public void OnMusicControl()
+{
+    audio.clip = Music;
+    if (audio.isPlaying == false)
+    {
+        audio.Play();
+    }
+    else
+    {
+        audio.Stop();
+    }
+    }
+    // Update is called once per frame
+   
+}
